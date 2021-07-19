9e4fafdbe1be4bfab4d3e8c4c0d4df57c53f4cda
anuchit <anuchit.nu567@gmail.com>
2021-07-19 23:40:01 +0700

    Lab2_1 success

---
+++ Lab2_1.java
@@ -0,0 +1,22 @@
+//Lab2_1 declare variable
+/*
+    Program by Anuchit Raksaphon
+    CTF010691
+    Fighting!!!!
+*/
+
+public class Lab2_1{
+    public static void main(String[] args) {
+        int weight=50;
+        boolean activated = true;
+        String name = "Anuchit";
+        float GPA = 2.86f;
+        char gender = 'm';
+
+        System.out.println("weight : "+weight);
+        System.out.println("activated : "+activated);
+        System.out.println("name : "+name);
+        System.out.println("GPA : "+GPA);
+        System.out.println("gender : "+gender);
+        }
+}
