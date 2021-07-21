887b9b73e719bd9c907b04b168cca8b542140893
anuchit <anuchit.nu567@gmail.com>
2021-07-21 19:02:19 +0700

    Lab6

---
+++ Lab6.java
@@ -0,0 +1,7 @@
+public class Lab6 {
+    public static void main(String[] args) {
+        char[] myChar={'A','n','u','c','h','i','t'};
+        String myWord = new String(myChar);
+        System.out.println(myWord);
+    }
+}
